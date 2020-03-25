283841b7702d49c7fd4cf66d3d7b9631767a24bf
9hafidz6 <9hafidz6@gmail.com>
2020-03-25 15:04:53 +0800

    modified project v1

--- project stuff/project_v1.0/main.c
+++ project stuff/project_v1.0/main.c
@@ -1,26 +1,27 @@
 #include "RTE_Components.h"
-#include  CMSIS_device_header
+#include CMSIS_device_header
 #include "cmsis_os2.h"
 
 #define MASK(x) (1 << (x))
-#define RED_LED 18		// PortB Pin 18 (LED RED)
-#define GREEN_LED 19		// PortB Pin 19 (LED GREEN)
-#define BLUE_LED 1		// PortD Pin 1  (LED BLUE)
+#define RED_LED 18					// PortB Pin 18 (LED RED)
+#define GREEN_LED 19				// PortB Pin 19 (LED GREEN)
+#define BLUE_LED 1					// PortD Pin 1  (LED BLUE)
 
-#define PTB0_Pin 0		// PortB Pin 0  (PWM)
-#define PTB1_Pin 1		// PortB Pin 1  (PWM)
+#define PTB0_Pin 0					// PortB Pin 0  (PWM)
+#define PTB1_Pin 1					// PortB Pin 1  (PWM)
 #define UART_TX_PORTE22 22	// PortE Pin 22 (BT TX)
 #define UART_RX_PORTE23 23	// PortE Pin 23 (BT RX)
 
-#define LED_MASK(x) (x & 0x06)
-#define BIT0_MASK(x) (x & 0x01)
 #define BAUD_RATE 9600
 
-uint8_t rx_data = 0x69;
+osEventFlagsId_t left_motor_flag;
+osEventFlagsId_t right_motor_flag;
+osEventFlagsId_t green_led_flag;
+osEventFlagsId_t red_led_flag;
 
 void InitGPIO(void){
 	// Enable Clock to PORTB and PORTD
-	SIM->SCGC5 |= ((SIM_SCGC5_PORTB_MASK) | (SIM_SCGC5_PORTD_MASK));
+	SIM->SCGC5 |= ((SIM_SCGC5_PORTB_MASK) | (SIM_SCGC5_PORTC_MASK) | (SIM_SCGC5_PORTD_MASK));
 	
 	// Configure MUX settings to make all 3 pins GPIO
 	PORTB->PCR[RED_LED] &= ~PORT_PCR_MUX_MASK;
@@ -30,8 +31,26 @@ void InitGPIO(void){
 	PORTD->PCR[BLUE_LED] &= ~PORT_PCR_MUX_MASK;
 	PORTD->PCR[BLUE_LED] |= PORT_PCR_MUX(1);
 	
+	PORTC->PCR[11] &= ~PORT_PCR_MUX_MASK;
+	PORTC->PCR[11] |= PORT_PCR_MUX(1);
+	PORTC->PCR[10] &= ~PORT_PCR_MUX_MASK;
+	PORTC->PCR[10] |= PORT_PCR_MUX(1);
+	PORTC->PCR[6] &= ~PORT_PCR_MUX_MASK;
+	PORTC->PCR[6] |= PORT_PCR_MUX(1);
+	PORTC->PCR[5] &= ~PORT_PCR_MUX_MASK;
+	PORTC->PCR[5] |= PORT_PCR_MUX(1);
+	PORTC->PCR[4] &= ~PORT_PCR_MUX_MASK;
+	PORTC->PCR[4] |= PORT_PCR_MUX(1);
+	PORTC->PCR[3] &= ~PORT_PCR_MUX_MASK;
+	PORTC->PCR[3] |= PORT_PCR_MUX(1);
+	PORTC->PCR[0] &= ~PORT_PCR_MUX_MASK;
+	PORTC->PCR[0] |= PORT_PCR_MUX(1);
+	PORTC->PCR[7] &= ~PORT_PCR_MUX_MASK;
+	PORTC->PCR[7] |= PORT_PCR_MUX(1);
+	
 	// Set Data Direction Registers for PortB and PortD
 	PTB->PDDR |= (MASK(RED_LED) | MASK(GREEN_LED));
+	PTC->PDDR |= (MASK(11)|MASK(10)|MASK(6)|MASK(5)|MASK(4)|MASK(3)|MASK(0)|MASK(7));
 	PTD->PDDR |= MASK(BLUE_LED);
 }
 
@@ -63,9 +82,12 @@ void initPWM(void) {
 	PORTB->PCR[PTB0_Pin] |= PORT_PCR_MUX(3);
 	PORTB->PCR[PTB1_Pin] &= ~PORT_PCR_MUX_MASK;
 	PORTB->PCR[PTB1_Pin] |= PORT_PCR_MUX(3);
+	PORTB->PCR[2] &= ~PORT_PCR_MUX_MASK;
+	PORTB->PCR[2] |= PORT_PCR_MUX(3);
 
 	// Enable Clock to TPM
 	SIM_SCGC6 |= SIM_SCGC6_TPM1_MASK;
+	SIM_SCGC6 |= SIM_SCGC6_TPM2_MASK;
 
 	// Select the MCGFLLCLK clock or MCGPLLCLK/2
 	SIM->SOPT2 &= ~SIM_SOPT2_TPMSRC_MASK;
@@ -74,7 +96,10 @@ void initPWM(void) {
 	// Interesting values
 	TPM1->MOD = 7500;	// 48MHz / (128 * 50)
 	TPM1_C0V = 3750;	// 7500 / 2
-
+	
+	TPM2->MOD = 7500;
+	TPM2_C0V = 3750;
+	
 	// Set clock and prescaler
 	TPM1->SC &= ~((TPM_SC_CMOD_MASK) | (TPM_SC_PS_MASK));
 	TPM1->SC |= (TPM_SC_CMOD(1) | TPM_SC_PS(7));
@@ -83,6 +108,15 @@ void initPWM(void) {
 	// Set ELSB and ELSA
 	TPM1_C0SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSB_MASK));
 	TPM1_C0SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
+	
+		// Set clock and prescaler
+	TPM2->SC &= ~((TPM_SC_CMOD_MASK) | (TPM_SC_PS_MASK));
+	TPM2->SC |= (TPM_SC_CMOD(1) | TPM_SC_PS(7));
+	TPM2->SC &= ~(TPM_SC_CPWMS_MASK);
+
+	// Set ELSB and ELSA
+	TPM2_C0SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSB_MASK));
+	TPM2_C0SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
 }
 
 void setFreq(int freq){
@@ -120,14 +154,12 @@ void Init_UART2(uint32_t baud_rate) {
 	UART2->C1 = UART2->S2 = UART2->C3 = 0;
 	
 	// Enable transmitter and receiver
-	UART2->C2 = UART_C2_TE_MASK | UART_C2_RE_MASK;
+	UART2->C2 = UART_C2_RE_MASK;
 	
 	// Enable Interrupt
 	NVIC_SetPriority(UART2_IRQn, 128);
 	NVIC_ClearPendingIRQ(UART2_IRQn);
 	NVIC_EnableIRQ(UART2_IRQn);
-	UART2->C2 |= UART_C2_TIE_MASK |
-	UART_C2_RIE_MASK;
 	UART2->C2 |= UART_C2_RIE_MASK;
 	//Q_Init(&TxQ);
 	//Q_Init(&RxQ);
@@ -140,24 +172,28 @@ void UART2_IRQHandler(void) {
 	}
 	if (UART2->S1 & UART_S1_RDRF_MASK) {
 		//When I receive data
-		rx_data = UART2->D;
-		if(LED_MASK(rx_data) == 2){
-			if(BIT0_MASK(rx_data)){
-				ledControl(1,3);
-				ledControl(2,3);
-				ledControl(3,3);
-				PORTB->PCR[PTB0_Pin] = PORT_PCR_MUX(3);
-				PORTB->PCR[PTB1_Pin] = PORT_PCR_MUX(3);
-			}
-			else{
-				ledControl(1,0);
-				ledControl(2,0);
-				ledControl(3,0);
-				PORTB->PCR[PTB0_Pin] = PORT_PCR_MUX(1);
-				PTB->PDOR &= ~MASK(PTB0_Pin);
-				PORTB->PCR[PTB1_Pin] = PORT_PCR_MUX(1);
-				PTB->PDOR &= ~MASK(PTB1_Pin);
-			}
+		int rx_data = UART2->D;
+		TPM1->SC |= TPM_SC_CMOD(1); //Enable PWM
+
+		switch(rx_data){
+			case 1: //TPM1_C0V = 750; //10%
+							//TPM2_C0V = 750;
+							osEventFlagsSet(left_motor_flag,0x0001);
+							osEventFlagsSet(right_motor_flag,0x0001);							
+							break;
+			case 2: TPM1_C0V = 2250; //30%
+							TPM2_C0V = 2250;
+							break;
+			case 3: TPM1_C0V = 3750; //50%
+							TPM2_C0V = 2250;
+							break;
+			case 4: TPM1_C0V = 5250; //70%
+							TPM2_C0V = 2250;
+							break;
+			case 5: TPM1_C0V = 6750; //90%
+							TPM2_C0V = 2250;
+							break;
+			default:TPM1->SC &= ~TPM_SC_CMOD(1); //Disable PWN
 		}
 	}
 	if (UART2->S1 & (UART_S1_OR_MASK | UART_S1_NF_MASK | UART_S1_FE_MASK | UART_S1_PF_MASK)) {
@@ -166,18 +202,65 @@ void UART2_IRQHandler(void) {
 	}
 }
 
+void delay(int d){
+	while(d--);
+}
+
+void tbrain(void *argument){
+	//if(rx_data == ){}
+	osEventFlagsSet(left_motor_flag,0x0001);
+	osEventFlagsSet(right_motor_flag,0x0001);		
+}
+void left_motor(void *argument){
+	for(;;){
+		osEventFlagsWait(left_motor_flag,0x0001, osFlagsWaitAny,osWaitForever);
+		TPM1_C0V = 750; //10%
+		TPM2_C0V = 750;
+	}
+}
+void right_motor(void *argument){
+	for(;;){
+		osEventFlagsWait(right_motor_flag,0x0001, osFlagsWaitAny,osWaitForever);
+		TPM1_C0V = 750; //10%
+		TPM2_C0V = 750;
+	}	
+}
+void red_led(void *argument){
+	//led control 
+}
+void green_led(void *argument){
+	int array[] = {11,10,6,5,4,3,0,7};
+	for (;;) {
+		for(int i=0; i<8; i++){
+			PTC->PDOR = MASK(array[i]);
+			osDelay(1000);
+		}
+	}	
+}
+
 int main (void) {
   // System Initialization
   SystemCoreClockUpdate();
 	InitGPIO();
 	initPWM();
 	Init_UART2(BAUD_RATE);
-	ledControl(1,0);
-	ledControl(2,0);
-	ledControl(3,0);
+	//ledControl(1,0);
+	//ledControl(2,0);
+	//ledControl(3,0);
  
-  //osKernelInitialize();                 // Initialize CMSIS-RTOS
+  osKernelInitialize();                 // Initialize CMSIS-RTOS
   //osThreadNew(app_main, NULL, NULL);    // Create application main thread
-  //osKernelStart();                      // Start thread execution
-  for (;;) {}
+	osThreadNew(tbrain,NULL,NULL);
+	osThreadNew(left_motor,NULL,NULL);
+	osThreadNew(right_motor,NULL,NULL);
+	osThreadNew(red_led,NULL,NULL);
+	osThreadNew(green_led,NULL,NULL);
+  osKernelStart();                      // Start thread execution
+  
+	left_motor_flag = osEventFlagsNew(NULL);
+	right_motor_flag = osEventFlagsNew(NULL);
+	red_led_flag = osEventFlagsNew(NULL);
+	green_led_flag = osEventFlagsNew(NULL);
+	
+	for (;;) {}
 }
